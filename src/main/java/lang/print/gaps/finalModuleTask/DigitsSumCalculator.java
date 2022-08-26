1821e44262e5d4f6d45f6e466b5b0c08b6fc5e1a
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-26 22:53:31 +0200

    task done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
+
     public void calculateSum(int number) {
+            int num1, num2, num3, num4, sum;
+//4321
+            num1 = number / 1000;
+            num2 = number % 1000/100;
+            num3 = number % 1000/10 % 10;
+            num4 = number % 1000%10;
+            sum = num1+num2+num3+num4;
+            System.out.println(sum);
+        }
     }
-}
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float temperatureFahrenheit = (temperatureCelsius * 1.8f) + 32.0f;
+        System.out.println(temperatureFahrenheit);
     }
 }
